fddbd0a730a725f45ee831e5dbf9cfbd16ab85ca
Jenny Knuth <inkboco@gmail.com>
2015-07-01 10:19:43 -0600

    files set up and dependencies installed

--- router.js
+++ router.js
@@ -1,5 +1,10 @@
 var routes = require('routes')(),
-    fs = require('fs')
+    fs = require('fs'),
+    qs = require('qs'),
+    db = require('monk')('localhost/music'), // Syncs up with mongo
+    songs = db.get('songs'), // grabs a collection from the music database
+    view = require('mustache'),
+    mime = require('mime')
 
 routes.addRoute('/songs', (req, res, url) => {
   res.setHeader('Content-Type', 'text/html')
