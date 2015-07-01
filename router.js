cb3520ea4a7609d50e9d66aa74d7fdf7f04f7910
Jenny Knuth <inkboco@gmail.com>
2015-07-01 11:42:17 -0600

    linted

--- router.js
+++ router.js
@@ -9,7 +9,7 @@ var routes = require('routes')(),
 routes.addRoute('/songs', (req, res, url) => {
   res.setHeader('Content-Type', 'text/html')
   if (req.method === 'GET') {
-    songs.find({}, {sort: {title: 1}}, function(err, docs) {
+    songs.find({}, {sort: {title: 1}}, function (err, docs) {
       if (err) console.log('no song')
       var file = fs.readFileSync('templates/songs/index.html')
       var template = view.render(file.toString(), {songs: docs}) // give property to docs, an array from mongo
@@ -19,12 +19,12 @@ routes.addRoute('/songs', (req, res, url) => {
   if (req.method === 'POST') {
     console.log(req.method)
     var data = ''
-    req.on('data', function(chunk) {
+    req.on('data', function (chunk) {
       data += chunk
     })
-    req.on('end', function(){
+    req.on('end', function () {
       var song = qs.parse(data)
-      songs.insert(song, function(err, doc) {
+      songs.insert(song, function (err, doc) {
         if (err) res.end('oops')
         res.writeHead(302, {'Location': '/songs'})
         res.end()
@@ -33,7 +33,7 @@ routes.addRoute('/songs', (req, res, url) => {
     console.log('Thanks for the data!')
   }
 })
-routes.addRoute('/songs/new', function (req,res,url) {
+routes.addRoute('/songs/new', function (req, res, url) {
   console.log(req.url)
   res.setHeader('Content-Type', 'text/html')
   if (req.method === 'GET') {
@@ -43,12 +43,11 @@ routes.addRoute('/songs/new', function (req,res,url) {
     res.end(template)
   }
 })
-routes.addRoute('/songs/:id', function(req, res, url) {
+routes.addRoute('/songs/:id', function (req, res, url) {
   console.log(req.url)
   res.setHeader('Content-Type', 'text/html')
   if (req.method === 'GET') {
-    console.log(req.method);
-    songs.findOne({_id: url.params.id}, function(err, doc) {
+    songs.findOne({_id: url.params.id}, function (err, doc) {
       if (err) res.end('It broke')
       var file = fs.readFileSync('templates/songs/show.html')
       var template = view.render(file.toString(), doc)
@@ -56,9 +55,9 @@ routes.addRoute('/songs/:id', function(req, res, url) {
     })
   }
 })
-routes.addRoute('/songs/:id/delete', (req, res, url) => {
+routes.addRoute('/songs/:id/delete', function (req, res, url) {
   if (req.method === 'POST') {
-    songs.remove({_id: url.params.id}, function(err, doc) {
+    songs.remove({_id: url.params.id}, function (err, doc) {
       if (err) console.log(err)
       res.writeHead(302, {'Location': '/songs'})
       res.end()
@@ -68,7 +67,7 @@ routes.addRoute('/songs/:id/delete', (req, res, url) => {
 routes.addRoute('/public/*', function (req, res, url) { // dynamically selecting public anything
   console.log(req.url)
   res.setHeader('Content-Type', mime.lookup(req.url)) // needed because of the splat
-  fs.readFile('.'+ req.url, function (err, file) { // the dynamic part for our static files
+  fs.readFile('.' + req.url, function (err, file) { // the dynamic part for our static files
     if (err) {
       res.setHeader('Content-Type', 'text/html')
       res.end('404')
