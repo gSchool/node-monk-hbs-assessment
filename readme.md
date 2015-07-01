78f95023923677314f7783fc802fcb059e62df71
Ryne Emory <ryneemory@gmail.com>
2015-07-01 11:11:57 -0600

    meets specs, now for some fun styling...

--- readme.md
+++ readme.md
@@ -6,6 +6,6 @@ To start the server, run `npm run start`.
 * Build an app that can create, read, and destroy `song` resources (edit / update is not needed)
 * Each song should have (and display) a title and length (in milliseconds)
 * Create routes & templates for song's `index` and `show` pages.
-* Include a delete button that removes a song on the index page.
+* Include a delete button that removes a song on the index page. ✔️
 * Use mustache for all templates
 * Include a stylesheet link (`/public/style.css`) in every template. It must include custom CSS of any sort.
--- router.js
+++ router.js
@@ -1,11 +1,76 @@
 var routes = require('routes')(),
-    fs = require('fs')
+    fs = require('fs'),
+    qs = require('qs'),
+    db = require('monk')('localhost/music')
+    songs = db.get('songs'),
+    mime = require('mime'),
+    view = require('mustache');
 
 routes.addRoute('/songs', (req, res, url) => {
   res.setHeader('Content-Type', 'text/html')
   if (req.method === 'GET') {
-
+    songs.find({}, function (err, docs) {
+      if (err) console.log(err);
+      var file = fs.readFileSync('templates/songs/index.html')
+      var template = view.render(file.toString(), {songs: docs})
+      res.end(template)
+    })
+  }
+  if (req.method === 'POST') {
+    var data = ''
+    req.on('data', function (chunk) {
+      data += chunk;
+    })
+    req.on('end', function() {
+      var song = qs.parse(data)
+      songs.insert(song, function (err, doc) {
+        if (err)  console.log(error);
+        res.writeHead(302, {'Location' :'/songs'})
+        res.end()
+      })
+    })
   }
 })
 
-module.exports = routes
+routes.addRoute('/songs/new', function (req, res, url) {
+  res.setHeader('Content-Type', 'text/html')
+  var file = fs.readFileSync('templates/songs/new.html');
+  var template = view.render(file.toString(), {})
+  res.end(template);
+})
+
+routes.addRoute('/public/*', function (req, res, url) {
+  res.setHeader('Content-Type', mime.lookup(req.url));
+  fs.readFile('.' + req.url, function (err, file) {
+    if (err) {
+      res.setHeader('Content-Type', 'text/html')
+      res.end('404')
+    }
+    res.end(file)
+  })
+})
+
+routes.addRoute('/songs/:id/delete', function (req, res, url) {
+  res.setHeader('Content-Type', 'text/html')
+  if (req.method === 'POST') {
+    songs.remove({_id: url.params.id}, function(err, doc) {
+      if (err) return err;
+      res.writeHead(302, {'Location': '/songs'})
+      res.end()
+    })
+  }
+})
+
+routes.addRoute('/songs/:id', function (req, res, url) {
+  res.setHeader('Content-Type', 'text/html')
+  if (req.method === 'GET') {
+    songs.findOne({_id: url.params.id}, function (err, doc) {
+      if (err) console.log (err)
+      var file = fs.readFileSync('templates/songs/show.html')
+      var template = view.render(file.toString(), doc)
+      res.end(template)
+    })
+  }
+})
+
+module.exports = routes;
