b7ae7e4cc3709b5aed548e0d8f3f7d89f1a2b183
Shannon Forsman <sforsman1@gmail.com>
2015-07-01 11:10:53 -0600

    updated assessment

--- router.js
+++ router.js
@@ -1,16 +1,16 @@
 var routes = require('routes')(),
-    fs = require('fs'),
-    mime = require('mime'),
-    db = require('monk')('localhost/music'),
-    songs = db.get('songs'),
-    view = require('mustache'),
-    qs = require('qs')
-
+  fs = require('fs'),
+  mime = require('mime'),
+  db = require('monk')('localhost/music'),
+  songs = db.get('songs'),
+  view = require('mustache'),
+  qs = require('qs')
 
 routes.addRoute('/songs', (req, res, url) => {
   res.setHeader('Content-Type', 'text/html')
   if (req.method === 'GET') {
-    songs.find({}, function(err, docs){
+    songs.find({}, function (err, docs) {
+      if (err) res.end('404')
       var file = fs.readFileSync('templates/songs/index.html')
       var template = view.render(file.toString(), {songs: docs})
       res.end(template)
@@ -33,7 +33,6 @@ routes.addRoute('/songs', (req, res, url) => {
   }
 })
 
-
 routes.addRoute('/songs/new', (req, res, url) => {
   res.setHeader('Content-Type', 'text/html')
   if (req.method === 'GET') {
@@ -46,7 +45,8 @@ routes.addRoute('/songs/new', (req, res, url) => {
 routes.addRoute('/songs/:id', (req, res, url) => {
   res.setHeader('Content-Type', 'text/html')
   if (req.method === 'GET') {
-    songs.findOne({_id: url.params.id}, function(err, doc){
+    songs.findOne({_id: url.params.id}, function (err, doc) {
+      if (err) res.end('404')
       var file = fs.readFileSync('templates/songs/show.html')
       var template = view.render(file.toString(), doc)
       res.end(template)
@@ -57,24 +57,22 @@ routes.addRoute('/songs/:id', (req, res, url) => {
 
 routes.addRoute('/songs/:id/delete', (req, res, url) => {
   if (req.method === 'POST') {
-    songs.remove({_id: url.params.id}, function(err, doc){
+    songs.remove({_id: url.params.id}, function (err, doc) {
+      if (err) throw err
       res.writeHead(302, {'Location': '/songs'})
       res.end()
     })
   }
 })
 
-
 routes.addRoute('/public/*', (req, res, url) => {
   res.setHeader('Content-Type', mime.lookup(req.url))
   if (req.method === 'GET') {
-    var file = fs.readFile('.'+ req.url, function(err, file){
+    fs.readFile('.' + req.url, function (err, file) {
       if (err) throw err
       res.end(file)
     })
   }
 })
-
-
 
 module.exports = routes
